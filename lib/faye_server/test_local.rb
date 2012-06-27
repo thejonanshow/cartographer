d8cbf890083d509d8d97d424a35383c9f58dad19
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-27 16:49:58 -0400

    Fix test loop

--- lib/faye_server/test_local.rb
+++ lib/faye_server/test_local.rb
@@ -3,9 +3,10 @@ require 'eventmachine'
 
   EM.run {
     client = Faye::Client.new('http://localhost:9000/faye')
+    foo = gets.chomp
     client.publish('/placemarkers', 
               { 'latitude' => rand * 179,
               'longitude' => rand * 179,
-              'text' => 'foo'
+              'text' => foo
               })
   }
